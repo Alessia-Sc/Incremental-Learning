b73c2c1cb969b4890607aff043b7d6414f6a5eb8
Alessia-Sc <64848396+Alessia-Sc@users.noreply.github.com>
2020-05-25 12:15:57 +0200

    Update icarl.py

--- icarl.py
+++ icarl.py
@@ -3,3 +3,20 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torch.nn import BCEWithLogitsLoss
 import numpy as np
+
+num_class=100
+lr=2
+weight_dec=0.2
+
+class iCaRL():
+  def __init__(self, k=2000,n_classes):
+    self.k=k
+    self.n_classes=n_classes
+  
+  def constructExemplar(self, data, net):
+    m=(self.k)/self.n_classes
+    
+    means=[]
+    exemplars=[]
+    
+    
